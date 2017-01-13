79311a665bded5f575e9cf7c7a166b5bfe77296a
Puchkov Ilya <puchkov@elewise.com>
2017-01-13 11:35:48 +0400

    cjplfk vjlekm

---
+++ EleWise.ELMA.Elmagram/EleWise.ELMA.Elmagram/ProjectAssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿[assembly: EleWise.ELMA.Modules.Attributes.AssemblyModule(EleWise.ELMA.Elmagram.__ModuleInfo.UID_S)]
+
+[assembly: EleWise.ELMA.Modules.Attributes.Module(EleWise.ELMA.Elmagram.__ModuleInfo.ModuleId, EleWise.ELMA.Elmagram.__ModuleInfo.UID_S)]
+[assembly: EleWise.ELMA.Modules.Attributes.ModuleTitle("EleWise.ELMA.Elmagram")]
+
+namespace EleWise.ELMA.Elmagram
+{
+
+    public static class __ModuleInfo
+    {
+        public const string ModuleId = "EleWise.ELMA.Elmagram";
+
+        public const string UID_S = "cafcd0b7-6587-4f19-aadd-061955cf14be";
+
+        public static readonly System.Guid UID = new System.Guid(UID_S);
+
+        public static string LocalizedName = EleWise.ELMA.SR.T("EleWise.ELMA.Elmagram");
+    }
+
+}
